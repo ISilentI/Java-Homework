9346ed2b421d15b05d456d2ae066b9a5850fe23e
ISilentI <93421826+ISilentI@users.noreply.github.com>
2021-11-20 21:36:14 +0300

    1

--- ZeroTask/src/Harmonic.java
+++ ZeroTask/src/Harmonic.java
@@ -1,7 +1,8 @@
 public class Harmonic {
 
     public static void main(String[] args){
-        
-
+        for (int i = 1; i <= 10; i++){
+            System.out.println("Number "+ i + ": " + "1/" + i);
+        }
     }
 }
