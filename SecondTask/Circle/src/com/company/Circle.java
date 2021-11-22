8dc39ba4e2b6a3b41fdc6835756b43cac07cf4d8
ISilentI <93421826+ISilentI@users.noreply.github.com>
2021-11-22 17:19:11 +0300

    1

--- SecondTask/Circle/src/com/company/Circle.java
+++ SecondTask/Circle/src/com/company/Circle.java
@@ -1,23 +1,46 @@
 package com.company;
 
 public class Circle {
-        int R, n;
-        double l, S, P;
+        private double R, n;
+        private double l = 0;
+        private String color;
 
-        public Circle (int R, int n){
+
+        public Circle (int R, int n, String color){
                 this.R = R;
                 this.n = n;
+                this.color = color;
         }
 
         public void Curve(int R, int n){
-                l = (3.14 * R * n) / 180;
+                this.l = 3.14 * R * n / 180;
         }
 
-        public double GetCurve(){
-                return this.l;
+        public void SetRadius(double R){
+                this.R = R;
+        }
+
+        public void SetAngle(double n) {
+                this.n = n;
+        }
+
+        public void setColor(String color) {
+                this.color = color;
+        }
+
+        public double getRadius() {
+                return R;
+        }
+
+        public double getAngle() {
+                return n;
+        }
+
+        public String getColor() {
+                return color;
         }
 
         public String toString(){
-                return "l: " + this.l + "R: " + R;
+                return "/Curve: " + this.l + " /Radius: " + this.R + " /Color: " + this.color;
         }
 }
--- SecondTask/Circle/src/com/company/CircleTest.java
+++ SecondTask/Circle/src/com/company/CircleTest.java
@@ -4,7 +4,8 @@ public class CircleTest {
     public CircleTest(){}
 
     public static void main(String[] args){
-            Circle obj1 = new Circle(10, 45);
+            Circle obj1 = new Circle(100, 45, "red");
+            obj1.Curve(100, 45);
             System.out.println(obj1);
     }
 }
