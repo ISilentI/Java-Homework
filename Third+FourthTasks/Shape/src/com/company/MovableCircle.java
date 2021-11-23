7824b618ef26c6c02802177aab0645cec988b2b3
ISilentI <93421826+ISilentI@users.noreply.github.com>
2021-11-23 18:18:52 +0300

    Update MovableCircle.java

--- Third+FourthTasks/Shape/src/com/company/MovableCircle.java
+++ Third+FourthTasks/Shape/src/com/company/MovableCircle.java
@@ -4,33 +4,32 @@ public class MovableCircle implements Movable {
     private int radius;
     private MovablePoint center;
 
-    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius)
-    {
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
         center = new MovablePoint(x, y, xSpeed, ySpeed);
         this.radius = radius;
     }
-    public String toString()
-    {
+
+    public String toString() {
         return "x = " + center.x + ", y = " + center.y + ", xSpeed = " + center.xSpeed + ", ySpeed = " + center.ySpeed;
     }
+
     @Override
-    public void MoveUp()
-    {
+    public void MoveUp() {
         center.y += center.ySpeed;
     }
+
     @Override
-    public void MoveDown()
-    {
+    public void MoveDown() {
         center.y -= center.ySpeed;
     }
+
     @Override
-    public void MoveLeft()
-    {
+    public void MoveLeft() {
         center.x -= center.xSpeed;
     }
+    
     @Override
-    public void MoveRight()
-    {
+    public void MoveRight() {
         center.x += center.xSpeed;
     }
 }
