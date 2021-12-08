cb55d9c8492a67e96cb4518b38e701f76cf68d04
ISilentI <93421826+ISilentI@users.noreply.github.com>
2021-12-08 18:56:15 +0300

    1

---
+++ 25-26Task/src/com/company/Stations.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Stations {
+    private String lineID, name;
+
+    public Stations(String lineID, String name) {
+
+        this.lineID = lineID;
+        this.name = name;
+    }
+}
